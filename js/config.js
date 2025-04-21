05be858dcd22ed6a171716d59e596525bff0989b
Meenakshi1402 <144078271+Meenakshi1402@users.noreply.github.com>
2025-04-20 22:17:56 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '',us-east-1_6puB40qDP
+        userPoolClientId: '', 2qphormlv2caamgr9l70624k24
         region: '' // e.g. us-east-2
     },
     api: {
